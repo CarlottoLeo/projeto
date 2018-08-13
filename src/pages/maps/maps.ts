e4c4b9863eb657eade2be014b5db863733b32839
CarlottoLeo <leo_carlotto@hotmail.com.br>
2018-08-13 20:14:15 -0300

    edit maps

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -37,6 +37,13 @@ export class MapsPage {
         map: this.map,
         title: "Hello World!"
       });
+
+      var markerMe = new google.maps.Marker({
+        position: position,
+        map: this.map,
+        title: "Hello World!"
+      });
+
       var contentString = '<span>Infomações</span>';
 
       var infowindow = new google.maps.InfoWindow({
@@ -49,37 +56,48 @@ export class MapsPage {
       infowindow.open(this.map, marker);
     });
 
+      google.maps.event.addListener(markerMe, 'click', function() {
+      infowindow.open(this.map, markerMe);
+    });
+
+    var myCoords = [
+      ['Eu', resp.coords.latitude, resp.coords.longitude, 1]
+    ];
+
     var locations = [
-      ['Eu', resp.coords.latitude, resp.coords.longitude, 4],
       ['<span>Eletricista</span>' + '<br>' + '<span>Disponivel</span>' + '<br>' + '<a href="##">Informações</a>', -27.6378422,-52.2723191, 3],
       ['Encanador', -27.645949, -52.261069, 2],
       ['Mecânico', -27.6378712,-52.274329, 1]
     ];
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
+      zoom: 16,
       center: new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
     var i;
 
-    var circulo;
-
     for (i = 0; i < locations.length; i++) {
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        icon: '../assets/img/prof.png',
+        map: map
+      });
+      markerMe = new google.maps.Marker({
+        position: new google.maps.LatLng(myCoords[0][1], myCoords[0][2]),
+        icon: '../assets/img/eu.png',
         map: map
       });
 
-      circulo = new google.maps.Circle(
+      new google.maps.Circle(
       {
           map: map,
-          center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+          center: new google.maps.LatLng(myCoords[0][1], myCoords[0][2]),
           radius: this.distance, // 1000 metros
           strokeColor: "blue",
           fillColor: "blue",
-          fillOpacity: 0.1,
+          fillOpacity: 0.1
       });
 
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
@@ -88,6 +106,14 @@ export class MapsPage {
           infowindow.open(map, marker);
         }
       })(marker, i));
+
+      google.maps.event.addListener(markerMe, 'click', (function(markerMe, i) {
+        return function() {
+          infowindow.setContent(myCoords[0][0]);
+          infowindow.open(map, markerMe);
+        }
+      })(markerMe, i));
+
     }
 
   }).catch((error) => {
